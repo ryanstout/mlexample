b9000e5c192b528773505fa29ce904515d7bdf32
Ryan Stout <ryanstout@gmail.com>
2012-04-09 11:45:21 -0600

    Update train script

--- lib/sentiment/train.rb
+++ lib/sentiment/train.rb
@@ -24,7 +24,7 @@ include_class "java.io.FileReader"
 include_class "weka.core.Utils"
 
 
-arff_file = File.dirname(__FILE__) + "/../../data/sentiment2.arff"
+arff_file = File.dirname(__FILE__) + "/../../data/sentiment.arff"
 
 # options = "weka.classifiers.meta.FilteredClassifier -F \"weka.filters.unsupervised.attribute.StringToWordVector -R first-last -P wv- -W 50000 -prune-rate -1.0 -T -N 0 -L -stemmer weka.core.stemmers.NullStemmer -M 1 -tokenizer \\\"weka.core.tokenizers.NGramTokenizer -delimiters \\\\\\\" \\\\\\\\\\r\\\\\\\\\\n\\\\\\\\\\t.,;:\\\\\\\\\\'\\\\\\\\\\\\\\\"()?!\\\\\\\" -max 2 -min 1\\\"\" -W weka.classifiers.functions.LibLINEAR -- -S 0 -C 1.0 -E 0.01 -B 1.0 -P"
 options = "weka.classifiers.meta.FilteredClassifier -F \"weka.filters.unsupervised.attribute.StringToWordVector -R first-last -P wv- -W 1000 -prune-rate -1.0 -T -N 0 -L -stemmer weka.core.stemmers.NullStemmer -M 1 \" -W weka.classifiers.functions.LibLINEAR -- -S 0 -C 1.0 -E 0.01 -B 1.0 -P"
@@ -33,6 +33,7 @@ arff = FileReader.new(arff_file)
 data = Instances.new(arff)
 data.setClassIndex data.num_attributes - 1 if data.class_index == -1
 
+# Do five fold cross validation
 train = data.trainCV(5, 1)
 test = data.testCV(5, 1)
 
@@ -44,36 +45,17 @@ classifier.set_options(opts)
 classifier.build_classifier(data)
 
 evaluation = Evaluation.new(train)
-# evaluation.evaluate_model(classifier, test)
 
 evaluation.crossValidateModel(classifier, train, 10, java.util.Random.new(1))
 
 puts evaluation.to_summary_string("\nResults\n======\n", false)
-# puts "Correct:"
-# puts evaluation.correct
-# puts "% Correct:"
-# puts evaluation.pct_correct
-# puts "Correlation Coefficient:"
-# puts evaluation.correlation_coefficient
-# puts "Mean Absolute Error:"
-# puts evaluation.mean_absolute_error
-
-# puts train.instance(0).to_s
 
 model_path = File.expand_path(File.join(File.dirname(__FILE__), "/../../models/sentiment.model"))
 
 puts "Percent Correct: " + evaluation.pct_correct.to_f.inspect
-if evaluation.pct_correct.to_f < 68.0
-  # Not good enough, lets not include this one, also delete the old model
-  puts "Not good enough, deleting #{model_path}"
-  if File.exists?(model_path)
-    File.delete(model_path)
-  end
-else
-  puts "Writing sentiment classifier to #{model_path}"
-  oos = java.io.ObjectOutputStream.new(java.io.FileOutputStream.new(model_path))
-  oos.writeObject(classifier)
-  oos.flush()
-  oos.close()
-  puts "Written"
-end
+puts "Writing sentiment classifier to #{model_path}"
+oos = java.io.ObjectOutputStream.new(java.io.FileOutputStream.new(model_path))
+oos.writeObject(classifier)
+oos.flush()
+oos.close()
+puts "Written"
