0abe7d075527b838f7992e8c82467f666dc9c3bd
Ryan Stout <ryanstout@gmail.com>
2012-04-09 11:43:15 -0600

    Fix weka path

--- lib/weka.sh
+++ lib/weka.sh
@@ -2,7 +2,7 @@
 
 # java -server -Xmx3000m -jar `dirname $0`/weka-3-6-4/weka.jar
 
-export WEKA_HOME=`pwd`/experiments/weka/weka-3-7-5/packages
+export WEKA_HOME=`pwd`/lib/weka-3-7-5/packages
 
 java -server -Xmx4000m -jar `dirname $0`/weka-3-7-5/weka.jar
 
